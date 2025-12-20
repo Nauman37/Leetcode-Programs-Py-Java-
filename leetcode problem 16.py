1826a2dcc83f4d91e0b82d3ded52b890583e23e1
N Nauman Ahmed <anauman757@gmail.com>
2025-12-21 00:38:40 +0530

    Add files via upload

---
+++ leetcode problem 16.py
@@ -0,0 +1,9 @@
+#algorithm to find the lowest value element in an array
+my_array = [7, 12, 9, 4, 11, 8]
+minVal = my_array[0]
+
+for i in my_array:
+    if i < minVal:
+        minVal = i
+
+print('Lowest value:', minVal)
